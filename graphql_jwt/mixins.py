fc51313ee9c93d52d1c9d93aafc9fc4b5f1647fe
Sverrir Arnórsson <sverrirarnors@gmail.com>
2021-08-08 21:25:56 +0000

    skoðum aðeins betur

--- graphql_jwt/mixins.py
+++ graphql_jwt/mixins.py
@@ -90,6 +90,7 @@ class KeepAliveRefreshMixin:
         token = jwt_settings.JWT_ENCODE_HANDLER(payload, context)
         signals.token_refreshed.send(sender=cls, request=context, user=user)
 
+        print(context.user)
         result = cls(payload=payload, refresh_expires_in=refresh_expires_in, user=context.user)
         return maybe_thenable((result, token), on_resolve)
 
